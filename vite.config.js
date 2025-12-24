5950e743f632e9af638c68d3f17609002e1afe5c
Rohithvishvin <rohith.n@vishvin.com>
2025-12-24 11:28:03 +0530

    24-12-2025

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    // Build output folder → supports Render deployment
+  build: {
+    outDir: "build"
+  },
+
 })
